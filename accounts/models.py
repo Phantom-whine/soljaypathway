e9adce3610684ae20a815f7f1f9fa9520cc89300
Paul jessey <ophantomo990@gmail.com>
2024-10-20 12:55:23 -0700

    saved changes

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from .managers import CustomManager
 
 class User(AbstractUser) :
     email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=10, unique=True)
+    country = models.CharField(max_length=20)
+    country_of_choice = models.CharField(max_length=250)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
